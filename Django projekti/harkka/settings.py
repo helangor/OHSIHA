f88cc3724f4a0eff516afae5be12229d6d333e27
helangor <henrikki_helander@hotmail.com>
2019-03-13 11:14:54 +0200

    Lisätty login ja logout navbariin

--- Django projekti/harkka/settings.py
+++ Django projekti/harkka/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'applikaatio',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = "applikaatio-home"
+LOGIN_URL = 'login'
--- Django projekti/users/views.py
+++ Django projekti/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,9 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Tili luotu käyttäjälle {username}!')
-            return redirect('applikaatio-home')
+            messages.success(request, f'Tilisi käyttäjälle {username} on luotu ja voit kirjautua sisään!')
+            return redirect('login')
     else: 
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
  
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')

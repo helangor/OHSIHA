d4b77ae819a1c3373b62f812f8b0e10c4a18fc6e
helangor <henrikki_helander@hotmail.com>
2019-03-13 16:01:49 +0200

    CRUD toiminnot toteutettu

--- Django projekti/applikaatio/models.py
+++ Django projekti/applikaatio/models.py
@@ -1,8 +1,8 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,3 +12,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return  reverse('post-detail', kwargs={'pk': self.pk})
+

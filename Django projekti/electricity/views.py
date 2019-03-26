ca3fc72bc76d5fc493eaca9d045d7b10b21da5d4
helangor <henrikki_helander@hotmail.com>
2019-03-26 19:02:35 +0200

    Ensimmäinen highcharts kokeilu

--- Django projekti/electricity/views.py
+++ Django projekti/electricity/views.py
@@ -2,16 +2,43 @@ from django.shortcuts import render
 from django.conf import settings
 import requests
 
+
+
 def electricity(request):
-    #response = requests.get('https://api.ipify.org/?format=json')
     response = requests.get(
-    url="https://api.fingrid.fi/v1/variable/event/json/188%2C181",
-    headers={
-        "x-api-key": settings.FINGRID_API_KEY,
-    },
+    url="https://api.fingrid.fi/v1/variable/event/json/192%2C193%2C188%2C181%2C191%2C189%2C201%2C205%2C202%2C194",
+    headers={"x-api-key": settings.FINGRID_API_KEY},
     )
 
     data = response.json()
-    return render(request, 'electricity/energy.html', {
-        'Value': data[0]['value'],
-    })
+    for x in range(10):
+        if data[x]['variable_id'] == 192:
+            tuotanto = data[x]['value']
+        elif data[x]['variable_id'] == 193:
+            kulutus = data[x]['value']
+        elif data[x]['variable_id'] == 194:
+            tuontivienti = data[x]['value']
+        elif data[x]['variable_id'] == 188:
+            ydinvoima = data[x]['value']
+        elif data[x]['variable_id'] == 181:
+            tuulivoima = data[x]['value']
+        elif data[x]['variable_id'] == 191:
+            vesivoima = data[x]['value']
+        elif data[x]['variable_id'] == 202:
+            yhteistuotanto = data[x]['value']
+        elif data[x]['variable_id'] == 201:
+            kauko = data[x]['value']
+        else:
+            muu = data[x]['value']
+
+    return render(request, 'electricity/energy.html', { 
+    'tuotanto' : tuotanto,
+    'kulutus' : kulutus,
+    'tuontivienti' : tuontivienti,
+    'ydinvoima' : ydinvoima,
+    'tuulivoima' : tuulivoima,
+    'vesivoima' : vesivoima,
+    'yhteistuotanto' : yhteistuotanto,
+    'kauko' : kauko,
+    'muu' : muu,
+        })
--- Django projekti/harkka/settings.py
+++ Django projekti/harkka/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5eri0g@6twd265iq7h$z#=o6g9bmbq_ti%b!2$kj74w!)(4vtu'
+#SECRET_KEY = '5eri0g@6twd265iq7h$z#=o6g9bmbq_ti%b!2$kj74w!)(4vtu'
+SECRET_KEY = config('SECRET_KEY', default='')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!

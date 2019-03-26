0d85452cfdf37477e5c0e68117b4128f0b01d426
helangor <henrikki_helander@hotmail.com>
2019-03-27 00:32:17 +0200

    Highshartilla lisää visualisointeja

--- Django projekti/electricity/urls.py
+++ Django projekti/electricity/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('electricity/', views.electricity, name='electricity-tuotto'),
+    #path('electricity/', views.electricity, name='electricity-tuotto'),
+    path('electricity/', views.timeline, name='electricity-tuotto'),
 ]
 
--- Django projekti/electricity/views.py
+++ Django projekti/electricity/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
+from datetime import datetime, timedelta
 import requests
 
 
-
+"""
 def electricity(request):
     response = requests.get(
     url="https://api.fingrid.fi/v1/variable/event/json/192%2C193%2C188%2C181%2C191%2C189%2C201%2C205%2C202%2C194",
@@ -11,7 +12,7 @@ def electricity(request):
     )
 
     data = response.json()
-    for x in range(10):
+    for x in range(len(data)):
         if data[x]['variable_id'] == 192:
             tuotanto = data[x]['value']
         elif data[x]['variable_id'] == 193:
@@ -42,3 +43,38 @@ def electricity(request):
     'kauko' : kauko,
     'muu' : muu,
         })
+"""
+
+def timeline(request):
+    timenow = datetime.now().strftime("%Y-%m-%d") + "T16%3A46%3A00%2B0000"
+    timelastyear = datetime.now() - timedelta(weeks = 25)
+    timelastyear = timelastyear.strftime("%Y-%m-%d") + "T16%3A46%3A00%2B0000"
+
+    urli='https://api.fingrid.fi/v1/variable/192/events/json?start_time={}&end_time={}'.format(timelastyear, timenow)
+    url2='https://api.fingrid.fi/v1/variable/193/events/json?start_time={}&end_time={}'.format(timelastyear, timenow)
+
+    response = requests.get(
+    url=urli,
+    headers={"x-api-key": 'uLwMWQp2Lp3RCOj3QjHLG6Bdhm3SjTglyE5d4WV5'},
+    )
+
+    data = response.json()
+    tuotantoarvot=[]
+    for i in range(0, len(data), 480):
+        tuotantoarvot.append(data[i]['value'])
+    
+    response = requests.get(
+    url=url2,
+    headers={"x-api-key": 'uLwMWQp2Lp3RCOj3QjHLG6Bdhm3SjTglyE5d4WV5'},
+    )
+
+    data = response.json()
+    kulutusarvot=[]
+    for i in range(0, len(data), 480):
+        kulutusarvot.append(data[i]['value'])
+
+    return render(request, 'electricity/energy.html', { 
+    'tuotantoajalta' : tuotantoarvot,
+    'kulutusajalta' : kulutusarvot,
+        })
+
